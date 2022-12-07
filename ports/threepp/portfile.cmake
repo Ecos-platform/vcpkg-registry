da4ee490e92fdd28e0f977697ab502a8dfcf8c7f
Lars Ivar Hatledal <laht@ntnu.no>
2022-12-07 12:29:11 +0100

    fix sha512

--- ports/threepp/portfile.cmake
+++ ports/threepp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
 	REPO markaren/threepp
 	REF 0cd0b155f4c9b8270d39adbb9f3e2bf0ea6a40e1
-	SHA512 0
+	SHA512 735f5d26aad45fe4ec31a63b4dc0b4dad3b0b81d310af6cdcfd9b45e1ec35ff8c97eca4538cc0994c9d2cc3d4581874fee59cb67e2658f973fccae00069052b4
     HEAD_REF master
 )
 
