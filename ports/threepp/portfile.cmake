15f0ef6300a910a968a5afa5978ae0e99f380ed0
Lars Ivar Hatledal <laht@ntnu.no>
2022-12-07 12:26:43 +0100

    Update threepp port

--- ports/threepp/portfile.cmake
+++ ports/threepp/portfile.cmake
@@ -1,11 +1,14 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO markaren/threepp
+	REPO markaren/threepp
+	REF 0cd0b155f4c9b8270d39adbb9f3e2bf0ea6a40e1
+	SHA512 0
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+	PREFER_NINJA
     OPTIONS
         -DTHREEPP_BUILD_EXAMPLES=OFF
 )
@@ -18,4 +21,7 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_copy_pdbs()
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(
+INSTALL "${SOURCE_PATH}/LICENSE" 
+DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" 
+RENAME copyright)
