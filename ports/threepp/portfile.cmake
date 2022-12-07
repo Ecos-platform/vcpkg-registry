97e85c80529ad1a9dc20f375bbf35ca544b82205
Lars Ivar Hatledal <laht@ntnu.no>
2022-12-07 13:29:43 +0100

    update port

--- ports/threepp/portfile.cmake
+++ ports/threepp/portfile.cmake
@@ -1,23 +1,24 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
 	REPO markaren/threepp
-	REF 0cd0b155f4c9b8270d39adbb9f3e2bf0ea6a40e1
-	SHA512 735f5d26aad45fe4ec31a63b4dc0b4dad3b0b81d310af6cdcfd9b45e1ec35ff8c97eca4538cc0994c9d2cc3d4581874fee59cb67e2658f973fccae00069052b4
-    HEAD_REF master
+	REF e210b3b15700bfb6f4b4c65337d3c9f800bd00b5
+	SHA512 ccd7a8ebf24bb8afcbc02a7fd913ca242c464392b068f83dbc1779c472bc27004a464b2167bb9c1b5f48a04183d751a890c11c304a0612021750acf7bd0a716a
+    HEAD_REF dev
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+		-DTHREEPP_BUILD_TESTS=OFF
         -DTHREEPP_BUILD_EXAMPLES=OFF
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(
-INSTALL "${SOURCE_PATH}/LICENSE" 
-DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" 
-RENAME copyright)
+	INSTALL "${SOURCE_PATH}/LICENSE" 
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" 
+	RENAME copyright)
