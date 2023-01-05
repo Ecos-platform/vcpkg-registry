1eee923f66138e950ef663fef9f6c7bcf6a483db
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2023-01-05 15:20:03 +0100

    update

--- ports/threepp/portfile.cmake
+++ ports/threepp/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
 	REPO markaren/threepp
-	REF e43b4b63701a313b4732789fedff9b3564ea07aa
+	REF 63524ffdfe06a103ed3d10e5d1c4f36759f57869
 	SHA512 9ca1a968b6c9bd9c6048f8b0829aee00dbeef0f907de6e674e79baf9b4ab80c73eb99a7b7579196cec6911b0265acb70ff0833db7e4c149e58454eaa4535d67b
-    HEAD_REF master
+    HEAD_REF vcpkg_test
 )
 
 vcpkg_cmake_configure(
