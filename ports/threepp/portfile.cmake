19977ed1b37749010361c85498cc878dfa256c31
Lars Ivar Hatledal <laht@ntnu.no>
2023-01-25 10:20:03 +0100

    update

--- ports/threepp/portfile.cmake
+++ ports/threepp/portfile.cmake
@@ -6,12 +6,20 @@ vcpkg_from_github(
     HEAD_REF vcpkg_test
 )
 
+vcpkg_from_github(
+    OUT_SOURCE_PATH GLTEXT_SOURCE_DIR
+    REPO https://github.com/vallentin/glText.git
+    REF 8200fa70e32acec0a3cd777d404f41ee0c203ca4
+    SHA512 f611326d75d6e87e58cb05e91f9506b1d83e6fd3b214fe311c4c15604feabfb7a18bbf9c4b4c389a39d615eb468b1f4b15802ab9f44f334a12310cb183fa77a7
+    HEAD_REF master
+)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
 		-DTHREEPP_BUILD_TESTS=OFF
         -DTHREEPP_BUILD_EXAMPLES=OFF
-		-DFETCHCONTENT_FULLY_DISCONNECTED=OFF
+		-Dgltext_SOURCE_DIR=${GLTEXT_SOURCE_DIR}
 )
 
 vcpkg_cmake_install()
