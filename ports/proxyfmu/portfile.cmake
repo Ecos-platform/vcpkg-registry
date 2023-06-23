54a33319b2920f61c3a992d6ba22a3fee86ba120
Lars Ivar Hatledal <laht@ntnu.no>
2023-06-23 12:17:55 +0200

    add proxyfmu

---
+++ ports/proxyfmu/portfile.cmake
@@ -0,0 +1,27 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+	REPO Ecos-platform/proxy-fmu
+	REF 43a1656b7f1c1e82fcf90cd863604b3e931bd441
+	SHA512 c61aa1e60e410269e227e3305570f1a3f9f0da2f3ef60c72786f6cfdbee897d3b4b857d4faccecb29b153083ecb04e234318ea6eaba4cc59976bdfdf208f9c0e
+    HEAD_REF vcpkg
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+		-DPROXYFMU_BUILD_TESTS=OFF
+		-DPROXYFMU_BUILD_EXAMPLES=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(
+	INSTALL "${SOURCE_PATH}/LICENSE" 
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" 
+	RENAME copyright)
